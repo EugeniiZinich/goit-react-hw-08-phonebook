dedcf83b67b7a41a761e3cc77a9ae01f1e55f2ba
EugeniiZinich <104194714+EugeniiZinich@users.noreply.github.com>
2023-02-04 16:27:59 +0000

    Deploying to gh-pages from @ EugeniiZinich/goit-react-hw-08-phonebook@7ff2ebe92b20400810ce84f7d887ea7bdb58c80d 🚀

---
+++ static/js/256.6e90dee6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{9256:function(e,t,n){n.r(t);var r=n(1087),c=n(184);t.default=function(){return(0,c.jsxs)("main",{style:{textAlign:"center"},children:[(0,c.jsx)("b",{style:{fontSize:60},children:"404"}),(0,c.jsx)("p",{children:"Page is no found"}),(0,c.jsx)(r.OL,{to:"/",children:"Back to HOME page"})]})}}}]);
+//# sourceMappingURL=256.6e90dee6.chunk.js.map
