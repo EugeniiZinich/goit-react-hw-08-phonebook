1a97f1e1b904e39befe0e674fae1daea3ca2fe3a
ZEugenii <zinich911@gmail.com>
2023-02-04 18:40:50 +0200

    add style

--- src/components/Home/Home.styled.jsx
+++ src/components/Home/Home.styled.jsx
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   position: absolute;
+  top: 50%;
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100vw;
-  height: 100vh;
+  /* height: 100vh; */
 `;
 
 export const Inner = styled.div`
