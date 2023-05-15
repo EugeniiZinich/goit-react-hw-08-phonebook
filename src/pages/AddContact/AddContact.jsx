f41cacb9d4088d191f1a8d511a24de2dbc48a09e
ZEugenii <zinich911@gmail.com>
2023-05-15 22:10:26 +0300

    add style to AddContact page

--- src/pages/AddContact/AddContact.jsx
+++ src/pages/AddContact/AddContact.jsx
@@ -3,7 +3,6 @@ import { useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import Particles from 'react-particles';
 import { loadFull } from 'tsparticles';
-import { Container } from '@mui/material';
 import { animateOptions } from 'components/AnimateBack/AnimateBack';
 import { addLocation } from 'redux/ContactsSlice/contactSlice';
 import { AddContactForm } from 'components/AddContactForm/AddContactForm';
